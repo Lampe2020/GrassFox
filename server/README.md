ca94094fcc563f395e99b08b2545279f0410f3e4
Lampe2020 <Stehlampe2020@gmail.com>
2024-07-13 03:56:42 +0200

    Remove subtrees entirely, instead place a README with a note in each subproject dir → I decided that would be easier than to track each subproject's changes in here also

--- server/README.md
+++ server/README.md
@@ -1,1 +1,3 @@
-# GrassFox Server
+# Note
+You need to add this project's code from [@Lampe2020's GitHub repo "gfserver"](https://github.com/Lampe2020/gfserver) 
+in order to use/build GrassFox!
--- update-subtrees.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-git subtree pull --prefix arrowey https://github.com/Lampe2020/arrowey.git main --squash
-git subtree pull --prefix l2m https://github.com/Lampe2020/l2m.git main --squash
-git subtree pull --prefix gss https://github.com/Lampe2020/gss.git main --squash
